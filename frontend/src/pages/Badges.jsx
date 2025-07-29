c77de9ba11872a80e2036a83183dc3d95a3689cc
Julie Kim <kimj25@oregonstate.edu>
2025-07-28 21:58:45 -0700

    Added tables, forms for trainers page and badges page

--- frontend/src/pages/Badges.jsx
+++ frontend/src/pages/Badges.jsx
@@ -1,9 +1,62 @@
-function Badges() {
-    return (
-        <>
-            <h1>Badges Page</h1>
-        </>
-    )
-} 
+import React from 'react';
+
+const Badges = () => {
+  return (
+    <div>
+      <h1>Badges in Kanto Region</h1>
+      
+      <table border="1">
+        <tr>
+          <th>ID</th>
+          <th>Badge Name</th>
+          <th>Gym Location</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Boulder Badge</td>
+          <td>Pewter City</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Cascade Badge</td>
+          <td>Cerulean City</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Thunder Badge</td>
+          <td>Vermillion City</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>Rainbow Badge</td>
+          <td>Celadon City</td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>Soul Badge</td>
+          <td>Fuchsia City</td>
+        </tr>
+        <tr>
+          <td>6</td>
+          <td>Marsh Badge</td>
+          <td>Saffron City</td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td>Volcano Badge</td>
+          <td>Cinnabar Island</td>
+        </tr>
+        <tr>
+          <td>8</td>
+          <td>Earth Badge</td>
+          <td>Viridian City</td>
+        </tr>
+      </table>
+      
+      <br/>
+      
+    </div>
+  );
+};
 
 export default Badges;
--- frontend/src/pages/Trainers.jsx
+++ frontend/src/pages/Trainers.jsx
@@ -1,9 +1,41 @@
-function Trainers() {
-    return (
-        <>
-            <h1>Trainers Page</h1>
-        </>
-    )
-} 
+import React from 'react';
+
+const Trainers = () => {
+  return (
+    <div>
+      <h1>Trainers</h1>
+      
+      <table border="1">
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Town</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Ash</td>
+          <td>Pallet Town</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Misty</td>
+          <td>Cerulean City</td>
+        </tr>
+      </table>
+      
+      <br/>
+      
+      <form>
+        <label>Trainer Name: </label>
+        <input type="text" />
+        <br/>
+        <label>Home Town: </label>
+        <input type="text" />
+        <br/>
+        <button type="button">Add Trainer</button>
+      </form>
+    </div>
+  );
+};
 
 export default Trainers;
