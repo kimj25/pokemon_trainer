501255547c955aaa194e0e18c5a384e814684dcb
Julie Kim <kimj25@oregonstate.edu>
2025-07-29 20:29:58 -0700

    Updated routes in Apps.jsx for trainers, badges, trainerbadges

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import Home from './pages/Home';
 import PokeDex from './pages/PokeDex';
 import Pokemon from './pages/Pokemons';
 import Types from './pages/Types';
+import Trainers from './pages/Trainers';
+import Badges from './pages/Badges';
+import TrainerBadges from './pages/TrainerBadges';
 import UpdatePokemonForm from './components/UpdatePokemonForm';
 
 // Components
@@ -26,6 +29,9 @@ function App() {
                 <Route path="/species" element={<PokeDex backendURL={backendURL} />} />
                 <Route path="/types" element={<Types backendURL={backendURL} />} />
                 <Route path="/pokemon/:id" element={<UpdatePokemonForm backendURL={backendURL} />} />
+                <Route path="/trainers" element={<Types backendURL={backendURL} />} />
+                <Route path="/badges" element={<Badges backendURL={backendURL} />} />
+                <Route path="/trainerbadges" element={<TrainerBadges backendURL={backendURL} />} />
             </Routes>
         </>
     );
